96c2d9caa3b28f4944639a230ed4a6e6fa0453bc
penny95 <49157799+penny95@users.noreply.github.com>
2019-11-29 23:39:38 +0300

    added a typeof operator

--- arraysort.js
+++ arraysort.js
@@ -4,25 +4,28 @@ function arraySort(list){
     let oddNumbers=[];
     let myCharacters=[];
    
+    if(typeof list=='string' || typeof list==='number'){
+        return 'invalid input'
+    }
+    else{
     for(let i=0; i<list.length; i++){
         if(list[i]%2===0){
             evenNumbers.push(list[i]);
-            obj['even']=evenNumbers.sort(function(a,b){return a-b});
+            obj['evens']=evenNumbers.sort(function(a,b){return a-b});
         }
 
         else if (list[i]%2===1){
 
         oddNumbers.push(list[i]);
-        obj['odd']=oddNumbers.sort(function(a,b){return a-b});
-    }
-    else {
-    myCharacters.push(list[i]);
-    obj['char']=myCharacters.sort();
-    }
+        obj['odds']=oddNumbers.sort(function(a,b){return a-b});
+      }
+       else {
+       myCharacters.push(list[i]);
+       obj['chars']=myCharacters.sort();
+     }
 }
    return obj;
 }
-
-console.log(arraySort([4, 9, 2, 6, 3, 5, 1, 'd', 'a', 'c', 'f', 8]));
+}
 
 module.exports = arraySort;
